d3492ff73042273585d40e94e22b7d84bdc9d00e
fepz <franpaez+github@gmail.com>
2020-04-29 09:05:39 -0300

    Permite especificar tiempos.

--- philo.c
+++ philo.c
@@ -1,5 +1,5 @@
 //
-// Ejercicio 4 -- cena de los filosofos
+// Problema de los filósofos comensales.
 //
 #include <pthread.h>
 #include <stdio.h>
@@ -7,25 +7,29 @@
 #include <unistd.h>
 #include <stdarg.h>
 
+// Mueve el cursor a la linea x, columna y (ver codigos ANSI).
+#define xy(x, y) printf("\033[%d;%dH", x, y)
+
+// Borra desde el cursor hasta el fin de la linea.
+#define clear_eol(x) print(x, 17, "\033[K")
+
+// Borra la pantalla.
+#define clear() printf("\033[H\033[J") 
+
 #define N 5
 const char *names[N] = { "Socrates", "Kant", "Spinoza", "Wittgenstein", "Popper" };
 
 #define M 5                     
-const char *topic[M] = { "Espagueti!", "Vida", "El ser", "Star Trek", "Futbol" };
+const char *topic[M] = { "Espagueti!", "Vida", "El Ser", "Netflix", "La verdad" };
 
-// Mueve el cursor a la linea x, columna y (ver codigos ANSI)
-#define xy(x, y) printf("\033[%d;%dH", x, y)
-
-// Borra desde el cursor hasta el fin de la linea
-#define clear_eol(x) print(x, 17, "\033[K")
-
-// Borra la pantalla 
-#define clear() printf("\033[H\033[J") 
+// Tiempos
+int segs_piensa = 1;     // intervalo de pensamiento en [1, seg_piensa] segundos.
+int segs_come = 1;
 
 // Mutex
 static pthread_mutex_t screen = PTHREAD_MUTEX_INITIALIZER;
 
-// Imprime en pantalla, en la posición x/y la cadena *fmt
+// Imprime en la posición (x,y) la cadena *fmt.
 void print(int y, int x, const char *fmt, ...)
 {
     va_list ap;
@@ -37,7 +41,7 @@ void print(int y, int x, const char *fmt, ...)
     pthread_mutex_unlock(&screen);
 }
 
-// El filosofo come
+// El filosofo come.
 void eat(int id)
 {
     int f[2]; // tenedores 
@@ -51,7 +55,7 @@ void eat(int id)
     print(id, 18, "..oO (necesito tenedores)");
     sleep(2);
 
-    // trata de tomar los tenedores
+    // Toma los tenedores.
     for (i = 0; i < 2; i++) {
         if (!i) {
             clear_eol(id);
@@ -59,18 +63,18 @@ void eat(int id)
 
         print(id, 18 + (f[i] != id) * 12, "tenedor%d", f[i]);
 
-        // espera para tomar el segundo tenedor
+        // Espera para tomar el segundo tenedor.
         sleep(3);
     }
 
-    // come durante un tiempo
+    // Come durante un tiempo.
     for (i = 0, ration = 3 + rand() % 8; i < ration; i++) {
         print(id, 40 + i * 4, "ñom");
-        sleep(1);
+        sleep(1 + (rand() % segs_come));
     }
 }
 
-// el filosofo piensa
+// El filosofo piensa.
 void think(int id)
 {
     int i, t;
@@ -79,17 +83,17 @@ void think(int id)
     do {
         clear_eol(id);
 
-        // piensa en algo...
+        // Piensa en algo...
         sprintf(buf, "..oO (%s)", topic[t = rand() % M]);
 
-        // imprime lo que piensa
+        // Imprime lo que piensa.
         for (i = 0; buf[i]; i++) {
             print(id, i + 18, "%c", buf[i]);
             if (i < 5)
                 sleep(1);
         }
 
-        sleep(2 + rand() % 2);
+        sleep(1 + rand() % segs_piensa);
     } while (t);
 }
 
@@ -103,13 +107,32 @@ void *filosofo(void *p)
     }
 }
 
-int main()
+int main(int argc, char* argv[])
 {
     int i;
-    int id[N]; // id para cada filosofo
-    pthread_t tid[N]; // hilos
+    int id[N]; // id para cada filosofo.
+    pthread_t tid[N]; 
 
-    clear();
+    if (argc != 3) {
+        fprintf(stderr, "Uso: %s segs-piensa segs-come\n", argv[0]);
+        fprintf(stderr, "\tsegs-piensa:\tmáxima cantidad de segundos que un filósofo puede estar pensando.\n");
+        fprintf(stderr, "\tsegs-come:\tmáxima cantidad de segundos que un filósofo puede estar comiendo.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    if ((segs_piensa = atoi(argv[1])) <= 0) {
+        fprintf(stderr, "segs-piensa debe ser mayor que cero.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    if ((segs_come = atoi(argv[2])) <= 0) {
+        fprintf(stderr, "segs-come debe ser mayor que cero.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    srand(getpid());
+
+    clear(); 
 
     for (i = 0; i < N; i++) {
         id[i] = i;
