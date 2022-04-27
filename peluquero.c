9a04abea380ef711cded44acb05c7dcd76635b7b
Francisco E. Paez <franpaez+github@gmail.com>
2022-04-27 10:51:42 -0300

    Agrega una descripción al programa peluquero.c

--- peluquero.c
+++ peluquero.c
@@ -1,7 +1,15 @@
 // 
-// Ejercicio 6 (opcional) -- peluquero
+// Ejercicio 5 -- El problema del peluquero dormido.
 //
-
+// Una peluquería tiene n peluqueros y m sillas donde los clientes esperan su 
+// turno. Si no hay clientes, los peluqueros duermen. Cuando arriba un cliente
+// alguno de los peluqueros se despierta y realiza el corte de pelo. Si todos 
+// los peluqueros estuvieran ocupados y hubiera sillas disponibles, el cliente 
+// se sienta a esperar su turno. Caso contrario, se retira. 
+//
+// Implementar un ejemplo empleando semáforos para manejar la sincronización
+// y mutexs para garantizar la exclusión mutua.
+//
 #include <stdio.h>
 #include <stdlib.h>
 
